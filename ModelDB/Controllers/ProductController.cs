a848870b6e2bddf08b9f515a0a0334ab17d3436f
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-01-19 11:59:17 +0300

    fdsfsfs

---
+++ ModelDB/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using ModelDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelDB.Controllers
+{
+    public class ProductController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Product
+        public ActionResult List()
+        {
+            MultiModel mm = new MultiModel();
+            mm.cList = db.Categories.ToList();
+            mm.pList = db.Products.ToList();
+            return View(mm);
+        }
+    }
+}
