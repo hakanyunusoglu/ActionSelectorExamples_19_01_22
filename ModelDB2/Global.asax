109f4278da1fd317d5c99bfcf562c332e71c26e7
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-01-19 12:12:05 +0300

    FDFD

---
+++ ModelDB2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModelDB2.MvcApplication" Language="C#" %>
---
+++ ModelDB2/Models/MultiClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelDB2.Models
+{
+    public class MultiClass
+    {
+        public List<Customer> cList { get; set; }
+        public List<Employee> eList { get; set; }
+    }
+}
