0a5dade2d9696e366832736a30d4bd18f64cee4a
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-01-19 10:28:10 +0300

    ljklj

--- CustomActionSelector/Controllers/ProductController.cs
+++ CustomActionSelector/Controllers/ProductController.cs
@@ -1,22 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 
 namespace CustomActionSelector.Controllers
 {
-    public class ProductController : Controller
+    public class Product
     {
-        public class Product
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+    }
+    public class ProductController : Controller
+    {       
+        public class AjaxMethodAttribute : ActionMethodSelectorAttribute
         {
-            public string ProductName { get; set; }
-            public int UnitPrice { get; set; }
+            public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+            {
+                return controllerContext.HttpContext.Request.IsAjaxRequest();
+            }
         }
         public ActionResult Index()
         {
             return View();
         }
+        [AjaxMethod]
         public JsonResult GetAll()
         {
             var products = new List<Product>
