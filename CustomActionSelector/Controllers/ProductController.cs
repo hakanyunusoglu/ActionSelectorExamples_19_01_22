3769e6632f08c5888084e6d4b6c85d93bf718591
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-01-19 10:05:17 +0300

    dsada

---
+++ CustomActionSelector/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomActionSelector.Controllers
+{
+    public class ProductController : Controller
+    {
+        public class Product
+        {
+            public string ProductName { get; set; }
+            public int UnitPrice { get; set; }
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAll()
+        {
+            var products = new List<Product>
+                {
+                new Product
+                {
+                    ProductName="Laptop",
+                    UnitPrice=8960
+                },
+                new Product
+                {
+                    ProductName="Masaüstü",
+                    UnitPrice=7600
+                }
+            };
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
